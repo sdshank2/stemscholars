4b8274446d7a31df3bf71add230e450d31c8255d
Siddharth Shankar <sdshank2@yahoo.com>
2025-03-24 15:37:49 -0400

    Update NavBar.jsx

--- gridlink/src/assets/NavBar.jsx
+++ gridlink/src/assets/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="sticky top-0 bg-white border-gray-200 dark:bg-green-700 z-50">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/stemscholars/home" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="GridLink Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GridLink</span>
                 </a>
